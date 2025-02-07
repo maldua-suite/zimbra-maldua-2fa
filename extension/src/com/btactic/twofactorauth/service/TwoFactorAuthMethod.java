92830eb841a864651505ab2b296e70740b4c23c9
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-02-07 17:51:30 +0100

    TwoFactorAuthMethod - Refactor (10)

--- extension/src/com/btactic/twofactorauth/service/TwoFactorAuthMethod.java
+++ extension/src/com/btactic/twofactorauth/service/TwoFactorAuthMethod.java
@@ -62,7 +62,7 @@ public abstract class TwoFactorAuthMethod {
         return zsc.jaxbToElement(response);
     }
 
-    protected abstract SendTwoFactorAuthCodeResponse.SendTwoFactorAuthCodeStatus doMethod(Element request, Map<String, Object> context)
+    private abstract SendTwoFactorAuthCodeResponse.SendTwoFactorAuthCodeStatus doMethod(Element request, Map<String, Object> context)
             throws ServiceException;
 
 }
