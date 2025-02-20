1358405c17db92fb9b82fab806f65eb23b7e4b7e
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-02-20 20:07:11 +0100

    Use new admin DisableTwoFactorAuth service.

--- extension/src/com/btactic/twofactorauth/ZetaTwoFactorAuthAdminService.java
+++ extension/src/com/btactic/twofactorauth/ZetaTwoFactorAuthAdminService.java
@@ -11,12 +11,14 @@ import com.zimbra.cs.service.admin.AdminService;
 import com.zimbra.common.soap.AdminConstants;
 
 import com.btactic.twofactorauth.service.admin.ClearTwoFactorAuthData;
+import com.btactic.twofactorauth.service.admin.DisableTwoFactorAuth;
 import com.btactic.twofactorauth.service.admin.GetClearTwoFactorAuthDataStatus;
 
 public class ZetaTwoFactorAuthAdminService extends AdminService implements DocumentService {
 
     public void registerHandlers(DocumentDispatcher dispatcher) {
         dispatcher.registerHandler(AdminConstants.CLEAR_TWO_FACTOR_AUTH_DATA_REQUEST, new ClearTwoFactorAuthData());
+        dispatcher.registerHandler(AdminConstants.DISABLE_TWO_FACTOR_AUTH_REQUEST, new DisableTwoFactorAuth());
         dispatcher.registerHandler(AdminConstants.GET_CLEAR_TWO_FACTOR_AUTH_DATA_STATUS_REQUEST, new GetClearTwoFactorAuthDataStatus());
     }
 
